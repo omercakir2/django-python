15691f6e53122f3b4cf1ae6453d9320ffb00cfc0
omercakir2 <omercakir2323@gmail.com>
2024-04-04 12:08:04 +0300

    add model

--- my_web_site/my_web_site/urls.py
+++ my_web_site/my_web_site/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('members.urls')),
+    path('', include('members.urls')), # we add apps' urls here
     path('admin/', admin.site.urls),
 ]
