3a33e9ad8b43f9b7a141a30c0f3a3981a9bf1243
omercakir2 <omercakir2323@gmail.com>
2024-04-06 12:09:32 +0300

    add notes how to add(insert) data into database

--- my_web_site/members/models.py
+++ my_web_site/members/models.py
@@ -5,3 +5,9 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
 # Create your models here.
 # Doing our jobs related to databases
+  
+#>>>Member.objects.all().values() to look data
+#>>> member = Member(firstname='Emil', lastname='Refsnes')
+#>>> member.save()
+  
+# '>>>' means you are workin in a python shell (execute this 'python3 manage.py shell')
