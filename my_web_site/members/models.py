107863a0552c58410ef7f42826a6c87c4b7bbfea
omercakir2 <omercakir2323@gmail.com>
2024-04-07 09:57:41 +0300

    add 2 models(columns)

--- my_web_site/members/models.py
+++ my_web_site/members/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
 # Create your models here.
 # Doing our jobs related to databases
-  
+
+
+#>>> from members.models import Member
 #>>>Member.objects.all().values() to look data
 #>>> member = Member(firstname='Emil', lastname='Refsnes')
 #>>> member.save()
