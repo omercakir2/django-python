96271cf501e60312c485382a79d786272041efd3
omercakir2 <omercakir2323@gmail.com>
2024-04-08 10:52:43 +0300

    add details page

--- my_web_site/members/urls.py
+++ my_web_site/members/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'), # this part is for indicating the url
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'), 
+    # this part is for indicating the url
     #that we use to access the app
 ]
