7ab17aed3340bfec581ca1986af8896ca74507be
omercakir2 <omercakir2323@gmail.com>
2024-04-09 13:29:37 +0300

    customize admin page

--- my_web_site/members/models.py
+++ my_web_site/members/models.py
@@ -5,6 +5,10 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
+
+def __str__(self):
+    
+    return f"{self.firstname} {self.lastname}"
 # Create your models here.
 # Doing our jobs related to databases
 
