772ba07723a8166502553fee66956c64a225678d
omercakir2 <omercakir2323@gmail.com>
2024-04-09 13:04:52 +0300

    add testing page

--- my_web_site/members/urls.py
+++ my_web_site/members/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'), 
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'), 
     # this part is for indicating the url
     #that we use to access the app
 ]
