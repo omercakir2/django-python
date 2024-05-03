fe572f70559b2455aa003fe033374f4e93f81920
omercakir2 <omercakir2323@gmail.com>
2024-05-03 18:41:59 +0300

    add signup post method and create db

--- Login-out-system/loginsystem/authentication/views.py
+++ Login-out-system/loginsystem/authentication/views.py
@@ -1,19 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.template import loader 
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
 
 def signup(request):
-    template = loader.get_template('signup.html')
-    return HttpResponse(template.render())
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request,"Your account is succesfully created !")
+        return redirect('signin')
+
+    #template = loader.get_template('signup.html')
+    #return HttpResponse(template.render())
+    return render(request,"signup.html")
 def signin(request):
-    template = loader.get_template('signin.html')
-    return HttpResponse(template.render())
+    #template = loader.get_template('signin.html')
+    #return HttpResponse(template.render())
+    return render(request,"signin.html")
 def signout(request):
-    template = loader.get_template('signout.html')
-    return HttpResponse(template.render())
+    #template = loader.get_template('signout.html')
+    #return HttpResponse(template.render())
+    return render(request,"signout.html")
     
