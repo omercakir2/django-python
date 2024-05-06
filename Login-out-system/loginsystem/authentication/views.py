52105dccf9298bd83021b54c5d2e8ccfa8d97478
omercakir2 <omercakir2323@gmail.com>
2024-05-06 13:58:02 +0300

    add function to authenticate the user

--- Login-out-system/loginsystem/authentication/views.py
+++ Login-out-system/loginsystem/authentication/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.template import loader 
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User #it is a ready-to-use data structure
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def home(request):
     template = loader.get_template('home.html')
@@ -32,9 +33,24 @@ def signup(request):
 def signin(request):
     #template = loader.get_template('signin.html')
     #return HttpResponse(template.render())
-    return render(request,"signin.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username,password=pass1)#checks username given matches with password
+        if user is not None:
+                login(request,user) 
+                fname = user.first_name
+                return render(request,"home.html",{'fname' : fname})  
+        else:
+             messages.error(request,"Not matched")
+             return redirect('home')
+    return render(request,"signin.html") 
 def signout(request):
     #template = loader.get_template('signout.html')
     #return HttpResponse(template.render())
-    return render(request,"signout.html")
+    logout(request)
+    messages.success(request,'Logged out succesfully')  
+
+    return redirect('home')
     
