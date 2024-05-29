ce717a6dfc83a52577eaa9e583ae2ea6575c8ec6
omercakir2 <omercakir2323@gmail.com>
2024-05-29 20:27:07 +0300

    load the old version due to db issues

--- Login-out-system/loginsystem/authentication/models.py
+++ Login-out-system/loginsystem/authentication/models.py
@@ -1,8 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-import uuid
 
-class CustomUser(AbstractUser):
-    is_verified = models.BooleanField(default=False)
-    verification_code = models.UUIDField(default=uuid.uuid4, editable=False)
-
+# Create your models here.
--- Login-out-system/loginsystem/authentication/urls.py
+++ Login-out-system/loginsystem/authentication/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import VerifyEmail
-
-
-
 
 urlpatterns = [
     path('',views.home,name="home"),
     path('signup',views.signup,name="signup"),
     path('signin',views.signin,name="signin"),
     path('signout',views.signout,name="signout"),
-    path('profile',views.profile,name="profile"),
-    path('verify-email/<int:user_id>/<uuid:verification_code>/', VerifyEmail.as_view(), name='verify_email'),
+    path('profile',views.profile,name="profile")
 ]
