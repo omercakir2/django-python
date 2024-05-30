8d0ae3fdac1ae7eac6bfb86ea08d10d126da8c4a
omercakir2 <omercakir2323@gmail.com>
2024-05-30 13:27:58 +0300

    extend usermodel with onetonefield

--- Login-out-system/loginsystem/authentication/models.py
+++ Login-out-system/loginsystem/authentication/models.py
@@ -1,3 +1,8 @@
+from email.policy import default
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Verificate(models.Model):
+    user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
+    is_verificate = models.BooleanField(default=False)
