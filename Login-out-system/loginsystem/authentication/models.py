8a4c1571753f142c45b09117a535430908961d5a
omercakir2 <omercakir2323@gmail.com>
2024-06-01 19:38:55 +0300

    add sending mail func(SSL error exists)

--- Login-out-system/loginsystem/authentication/models.py
+++ Login-out-system/loginsystem/authentication/models.py
@@ -1,8 +1,10 @@
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 # Create your models here.
 
 class Verificate(models.Model):
     user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
     is_verificate = models.BooleanField(default=False)
+    verification_code = models.UUIDField(default=uuid.uuid4, editable=False)
