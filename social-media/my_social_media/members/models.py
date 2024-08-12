938380a244962347bd1826772c5d938a390ffc44
omercakir2 <omercakir2323@gmail.com>
2024-08-12 15:14:13 +0300

    create table to store users

--- social-media/my_social_media/members/models.py
+++ social-media/my_social_media/members/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    
+# the actual code that being exucuted is this:
+# CREATE TABLE "members_member" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "firstname" varchar(255) NOT NULL, "lastname" varchar(255) NOT NULL); COMMIT;
+
+    
+    
+
