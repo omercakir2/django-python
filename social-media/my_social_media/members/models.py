00056c4ad077d220e5971c35b04df8b5b07def27
omercakir2 <omercakir2323@gmail.com>
2024-08-12 15:35:53 +0300

    make some changes on db with sqlite

--- social-media/my_social_media/members/models.py
+++ social-media/my_social_media/members/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    join_date= models.DateField(null=True)
+    phone = models.IntegerField(null=True)
     
 # the actual code that being exucuted is this:
 # CREATE TABLE "members_member" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "firstname" varchar(255) NOT NULL, "lastname" varchar(255) NOT NULL); COMMIT;
