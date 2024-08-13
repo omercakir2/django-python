4bfb2ae4e52205b322732ad0cc8074114191512b
omercakir2 <omercakir2323@gmail.com>
2024-08-13 13:17:19 +0300

    set debug false

--- social-media/my_social_media/my_social_media/settings.py
+++ social-media/my_social_media/my_social_media/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z-dx)&#%x2ji0uw^cplj_fqw%stww6i7&x$*3yojo#%@=a#ih^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
