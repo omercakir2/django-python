e9276ea27a2e223abc6a402e0f0ff7be3034c5ca
omercakir2 <omercakir2323@gmail.com>
2024-08-13 13:04:37 +0300

    create home and details page

--- social-media/my_social_media/members/views.py
+++ social-media/my_social_media/members/views.py
@@ -2,9 +2,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Member
 def members(request):
-  template = loader.get_template('firsthtml.html')
-  return HttpResponse(template.render())
+  members = Member.objects.all().values()    
+  template = loader.get_template('allmembers.html')
+  context = {
+      'mymembers':members,
+  }
+  return HttpResponse(template.render(context,request))
+
+def details(request,id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember':mymember,
+    }
+    return HttpResponse(template.render(context,request))
+
+def home(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+    
 
 
