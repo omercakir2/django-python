cd1ab28d3738d47d8a01f32c3151a33b87672dfb
omercakir2 <omercakir2323@gmail.com>
2024-08-27 11:16:08 +0300

    django messages add

--- social-media/my_social_media/my_social_media/settings.py
+++ social-media/my_social_media/my_social_media/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z-dx)&#%x2ji0uw^cplj_fqw%stww6i7&x$*3yojo#%@=a#ih^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','*']
 
 
 # Application definition
