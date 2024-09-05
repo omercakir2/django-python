658bb49b42a426a704e0f441b96f501d6b28a390
omercakir2 <omercakir2323@gmail.com>
2024-09-05 14:26:22 +0300

    install Pillow to handle images

--- social-media/my_social_media/members/urls.py
+++ social-media/my_social_media/members/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import register_view, login_view, logout_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.home,name='home'),
@@ -13,7 +15,8 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('login/forget-password',views.forget_password_view,name='forget_password'),
     path('reset-password/<uidb64>/<token>/', views.reset_password_view, name='reset_password'),
-    path('members/details/<int:id>/edit',views.edit_view,name='edit')
+    path('members/details/<int:id>/edit',views.edit_view,name='edit'),
+    path('creator/',views.about_me_view,name='about_me'),
 
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
